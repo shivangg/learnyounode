b3fef581b3485017c4dbf8d552e2807eaec1b2b5
shivangg <xshivangx@gmail.com>
2019-01-23 17:28:41 +0530

    feat: Sync and Async line counter

---
+++ challenge3.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+let buf = fs.readFileSync(process.argv[2]);
+
+let fileContents = buf.toString();
+
+console.log(fileContents.split('\n').length - 1)
---
+++ challenge4.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+let fileContents = undefined;
+
+let lineFinder = () => {fs.readFile(process.argv[2], (err, data) => {
+
+  if (err) {
+    console.log(err);
+
+  }
+
+  fileContents = data.toString().split('\n').length - 1;
+  console.log(fileContents);
+})};
+
+lineFinder();
+
