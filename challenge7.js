2f72da2630fc4d1b663a01d9288f5c612d102f81
shivangg <xshivangx@gmail.com>
2019-01-24 16:50:58 +0530

    feat: Working http.get with Promise

--- challenge7.js
+++ challenge7.js
@@ -1,7 +1,30 @@
 const http = require('http');
+const util = require('util');
 
-http.get(process.argv[2], (res) => {
-  res.setEncoding('utf8');
-  res.on('data',console.log);
-  res.on('error', console.error);
-}).on('error', console.error);
+const get = util.promisify(http.get);
+
+// http.get(process.argv[2], (res) => {
+//   res.setEncoding('utf8');
+//   res.on('data', console.log);
+//   res.on('error', console.error);
+// }).on('error', console.error);
+
+function getData(url) {
+  return new Promise((resolve, reject) => {
+    http.get(url, (res) => {
+      res.setEncoding('utf8');
+      res.on('data', (data) => {
+        console.log(data);
+      });
+      res.on('error', reject);
+    }).on('error', reject);
+  });
+}
+
+getData(process.argv[2])
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.error(err);
+  })
--- challenge8.js
+++ challenge8.js
@@ -1,13 +1,24 @@
 const http = require('http');
 const bl = require('bl');
 
-http.get(process.argv[2], (res) => {
-  res.pipe(bl( (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-    data = data.toString();
+function getData(url) {
+  return new Promise((resolve, reject) => {
+    http.get(url, (res) => {
+      res.pipe(bl((err, data) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data.toString());
+      }))
+    });
+  })
+}
+
+getData(process.argv[2])
+  .then( data => {
     console.log(data.length);
     console.log(data);
-  } ))
-});
+  })
+  .catch( err => {
+    console.error(err);
+  } )
